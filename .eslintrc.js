afdd9c482e77dfe0138495e40b1271137b8b86e9
Gary Bortosky <garybort@gmail.com>
2019-06-27 23:02:38 -0500

    customize indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 'use strict';
 
-// const FriendlyFormatter = require('eslint-formatter-friendly');
-// const stylish = require('eslint/lib/formatters/stylish')
-
 module.exports = {
-  // formatter: FriendlyFormatter,
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
   ],
   plugins: ['@typescript-eslint'],
-  rules: { // works with rules like this
-    'sort-keys': 2,
-    '@typescript-eslint/explicit-function-return-type': 2,
+  rules: {
+    'sort-keys': "error",
+    '@typescript-eslint/explicit-function-return-type': "error",
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2]
   },
   env: {
     browser: true,
